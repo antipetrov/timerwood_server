79bc6d9d9523eaffcb42d8cec5351af0646ee8c6
Петр Матюхов <petrov@relishgames.com>
2014-02-17 21:58:10 +0400

    removed views file

--- twoodapp/__init__.py
+++ twoodapp/__init__.py
@@ -15,21 +15,6 @@ app.config.from_object(settings)
 db = SQLAlchemy(app)
 
 
-# db models
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    login = db.Column(db.String(64), index=True, unique=True)
-    code = db.Column(db.String(64), index=True, unique=True)
-
-class Timer(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    create_time = db.Column(db.DateTime(), index=True)
-    edit_time = db.Column(db.DateTime(), index=True)
-    master_code = db.Column(db.String(64), index=True)
-    guest_code =  db.Column(db.String(64), index=True)
-    timer_data = db.Column(db.Text)
-
-
 # api entry points
 from flask.views import View, MethodView
 
